2722b31c1fda4c49b8ba1541023bb484a59eeed9
Fatima <bc220417002fzn@vu.edu.pk>
2026-02-05 23:28:48 +0500

    updated cookies

--- utils/generatejwttokenandsetcookies.js
+++ utils/generatejwttokenandsetcookies.js
@@ -3,12 +3,14 @@ import jwt from 'jsonwebtoken'
 export const GenerateJwtTokenAndSetCookiesEmployee = (res, EMid, EMrole, ORGID) => {
     const token = jwt.sign({ EMid, EMrole, ORGID }, process.env.JWT_SECRET, { expiresIn: '7d' })
 
-    res.cookie("EMtoken", token, {
+    const cookieOptions = {
         maxAge: 7 * 24 * 60 * 60 * 1000,
         httpOnly: true,
-        secure: true,
-        sameSite: "none",
-    })
+        secure: process.env.NODE_ENV === 'production',
+        sameSite: process.env.NODE_ENV === 'production' ? "none" : "lax",
+    };
+
+    res.cookie("EMtoken", token, cookieOptions)
 
     return token
 }
